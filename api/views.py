6e860089bfec119c6964c66e4213c3f0e8ec130a
Ykyki4 <jimmypantalones@gmail.com>
2023-03-04 09:26:36 +0300

    Add dishdetailserializer to view
    Also add retrieve to tagview

--- api/views.py
+++ api/views.py
@@ -3,23 +3,31 @@ from django.shortcuts import get_object_or_404
 from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework.response import Response
 
-from .serializers import TagSerializer, DishSerializer
+from .serializers import TagSerializer, TagDetailSerializer, DishSerializer, DishDetailSerializer
 from .models import Tag, Dish
  
-class TagView(ModelViewSet):
-    serializer_class = TagSerializer
-    queryset = Tag.objects.all()
+class TagView(ViewSet):
+    def list(self, request):
+        queryset = Tag.objects.all()
+        serializer = TagSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def retrieve(self, request, pk=None):
+        queryset = Tag.objects.all()
+        dish = get_object_or_404(queryset, id=pk)
+        serializer = TagSerializer(dish)
+        return Response(serializer.data)
     
 
 class DishView(ViewSet):
     def list(self, request):
-        queryset = Dish.objects.all().order_by('-views')
+        queryset = Dish.objects.all()
         serializer = DishSerializer(queryset, many=True)
         return Response(serializer.data)
     
     def retrieve(self, request, pk=None):
         queryset = Dish.objects.all()
         dish = get_object_or_404(queryset, id=pk)
-        serializer = DishSerializer(dish)
+        serializer = DishDetailSerializer(dish)
         return Response(serializer.data)
     
