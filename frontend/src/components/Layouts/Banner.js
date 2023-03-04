1541ab43dbe3a46126d2eacc1de46429b0b3f37b
Ykyki4 <jimmypantalones@gmail.com>
2023-03-04 14:32:27 +0300

    centered text in banner

--- frontend/src/components/Layouts/Banner.js
+++ frontend/src/components/Layouts/Banner.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 
 const Banner = ({text, size="text-7xl"}) => {
     return (
-        <div className="flex justify-center mt-16 p-8 rounded-full bg-blue-100">
-            <h1 className={`font-sans font-bold text-cyan-900 ${size}`}>{text}</h1>
+        <div className="mt-16 p-8 rounded-full bg-blue-100">
+            <p className={`text-center font-sans font-bold text-cyan-900 ${size}`}>{text}</p>
         </div>
     );
 }
