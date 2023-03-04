f9d85613160dc1398793d502b05f14a503173225
Ykyki4 <jimmypantalones@gmail.com>
2023-03-04 09:25:26 +0300

    add dishes serializers to tags
    Also add dishproduct serializers and dishstep serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,17 +1,53 @@
 from rest_framework import serializers
  
-from .models import Tag, Dish
- 
- 
-class TagSerializer(serializers.ModelSerializer):
- 
+from .models import Tag, Dish, DishProduct, DishStep
+
+
+class DishProductSerializer(serializers.ModelSerializer):
+    title = serializers.SerializerMethodField('get_product_title')
+    
+    def get_product_title(self, obj):
+        return obj.product.title
+    
     class Meta:
-        model = Tag
-        fields = ('id', 'title')
+        model = DishProduct
+        fields = ('id', 'title', 'amount')
+
+
+class DishStepSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = DishStep
+        fields = ('id', 'order', 'picture', 'description')
+
+
+class DishDetailSerializer(serializers.ModelSerializer):
+    dish_products = DishProductSerializer(many=True)
+    steps = DishStepSerializer(many=True)
+
+    class Meta:
+        model = Dish
+        fields = ('id', 'recommended', 'title', 'description', 'dish_products', 'steps', 'tag', 'picture', 'views')
         
 
 class DishSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = Dish
         fields = ('id', 'recommended', 'title', 'description', 'tag', 'picture', 'views')
+
+        
+class TagSerializer(serializers.ModelSerializer):
+    dishes = DishSerializer(many=True)
+ 
+    class Meta:
+        model = Tag
+        fields = ('id', 'title', 'dishes')
+        
+
+class TagDetailSerializer(serializers.ModelSerializer):
+    dishes = DishDetailSerializer(many=True)
+ 
+    class Meta:
+        model = Tag
+        fields = ('id', 'title', 'dishes')
