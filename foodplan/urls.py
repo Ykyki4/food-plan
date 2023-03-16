11cb36efedb4a28ff60f36fed954e418f175b0c7
Ykyki4 <jimmypantalones@gmail.com>
2023-03-16 11:03:32 +0300

    remake api urls

--- foodplan/urls.py
+++ foodplan/urls.py
@@ -2,18 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-
-from api.views import TagView, DishView
-
-router = DefaultRouter()
-
-router.register(r'tags', TagView, 'tag')
-router.register(r'dishes', DishView, 'dish')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('frontend.urls')),
-    path('api/', include(router.urls)),
+    path('api/', include('api.urls')),
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
 +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
