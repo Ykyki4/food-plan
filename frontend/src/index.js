471785938152289d1d4c25724f99b76ecdda345f
Ykyki4 <jimmypantalones@gmail.com>
2023-03-16 11:02:25 +0300

    remove unused render from index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import App from "./components/App"
 import React from "react";
-import { render } from "react-dom";
 import ReactDOM from "react-dom/client";
 
 const app = ReactDOM.createRoot(document.getElementById('app'));
