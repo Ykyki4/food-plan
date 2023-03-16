99d0fdc960e80e6c53924bf0ffb7cc0473cc7fce
Ykyki4 <jimmypantalones@gmail.com>
2023-03-16 18:26:28 +0300

    fix debug env

--- foodplan/settings.py
+++ foodplan/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = env("DEBUG", False)
+DEBUG = env.bool("DEBUG", False)
 
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", ["127.0.0.1", "localhost"])
